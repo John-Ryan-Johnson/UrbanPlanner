35540baaaadc61be8d23e9e4cced8a77c8c3ef76
John-Ryan-Johnson <john4johnkids@gmail.com>
2020-08-09 17:14:40 -0500

    Add project files.

---
+++ UrbanPlanner/Building.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using System.Text;
+
+namespace UrbanPlanner
+{
+    class Building
+    {
+        //properties
+        public int Stories { get; set; }
+        public double Width { get; set; }
+        public double Depth { get; set; }
+        public double Volume { get; private set; }
+
+        //fields
+        private string _designer = "John Johnson";
+        private DateTime _dateConstructed = new DateTime();
+        private string _address = "";
+        private string _owner = "";
+
+        //constructor
+        public Building(string address)
+        {
+            this._address = address;
+        }
+
+        //methods
+        public void Construct()
+        {
+            _dateConstructed = DateTime.Now;
+            Volume = 3 * Stories * Depth * Width;
+        }
+
+        public void Purchase(string owner)
+        {
+            _owner = owner;
+            Console.WriteLine($"{_address}------\n" +
+                $"Designed by {_designer}\n" +
+                $"Constructed on {_dateConstructed}\n" +
+                $"Owned by {_owner}\n" +
+                $"{Volume} cubic meters of space\n"
+                );
+        }
+
+
+    }
+
+}
---
+++ UrbanPlanner/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace UrbanPlanner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Building FiveOneFiveZero = new Building("5150 Van Halen Blvd");
+            FiveOneFiveZero.Width = 100;
+            FiveOneFiveZero.Depth = 50;
+            FiveOneFiveZero.Stories = 10;
+
+            Building TwoOneFour = new Building("214 Main Street");
+            TwoOneFour.Width = 50;
+            TwoOneFour.Depth = 50;
+            TwoOneFour.Stories = 4;
+
+            Building OneZeroOne = new Building("101 NewFound Rd.");
+            OneZeroOne.Width = 30;
+            OneZeroOne.Depth = 25;
+            OneZeroOne.Stories = 1;
+
+            FiveOneFiveZero.Construct();
+            TwoOneFour.Construct();
+            OneZeroOne.Construct();
+
+            FiveOneFiveZero.Purchase("Sammy Hagar");
+            TwoOneFour.Purchase("Gordon Gecko");
+            OneZeroOne.Purchase("Brian Johnson");
+        }
+    }
+}
